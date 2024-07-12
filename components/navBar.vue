20bb758707b890f555706f3786d7568352e3a047
sad-panda-dev <80590323+sad-panda-dev@users.noreply.github.com>
2024-07-12 13:39:07 -0400

    Changed to npm

---
+++ components/navBar.vue
@@ -0,0 +1,75 @@
+<template>
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="#">
+          Supertramp
+        </a>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <nuxt-link class="navbar-item" to="/">About</nuxt-link>
+          <nuxt-link class="navbar-item" to="/destinations">Destinations</nuxt-link>
+          <nuxt-link class="navbar-item" to="/categories">Categories</nuxt-link>
+          <nuxt-link class="navbar-item" to="/resources">Resources</nuxt-link>
+          <nuxt-link class="navbar-item" to="/contact">Contact</nuxt-link>
+        </div>
+        <div class="navbar-end">
+          <a class="navbar-item" href="#"><i class="fa-brands fa-facebook-f"></i></a>
+          <a class="navbar-item" href="#"><i class="fa-brands fa-twitter"></i></a>
+          <a class="navbar-item" href="#"><i class="fa-brands fa-instagram"></i></a>
+        </div>
+      </div>
+    </nav>
+</template>
+  
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    name: 'NavBar',
+  });
+</script>
+  
+<style scoped>
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 2rem;
+    background-color: #fff;
+    border-bottom: 1px solid #eaeaea;
+  }
+  
+  .navbar-brand .navbar-item {
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #000;
+    text-decoration: none;
+  }
+  
+  .navbar-menu {
+    display: flex;
+    align-items: center;
+  }
+  
+  .navbar-start .navbar-item,
+  .navbar-end .navbar-item {
+    margin-right: 1rem;
+    color: #000;
+    text-decoration: none;
+  }
+  
+  .navbar-start .navbar-item.active,
+  .navbar-start .navbar-item:hover {
+    border-bottom: 2px solid #00aaff;
+  }
+  
+  .navbar-end .navbar-item i {
+    font-size: 1.2rem;
+  }
+  
+  .navbar-end .navbar-item:hover i {
+    color: #00aaff;
+  }
+</style>
+  
---
+++ pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+      <NavBar />
+      <!-- Rest of your content goes here -->
+      <section>
+        <!-- Example of main content -->
+        <div class="main-content">
+          <img src="../assets/micael-rodrigues-G3XXUyKYmHo-unsplash.jpg" alt="Navigating Mountains In Durmitor National Park" class="hero-image">
+          <div class="overlay">
+            <h2>Navigating Mountains In Durmitor National Park</h2>
+            <p>When we showed up in Montenegro, a country that literally translates as "Black Mountain," we were determined to spend some time exploring the peaks and crags around the place.</p>
+            <a class="btn" href="#">Read</a>
+          </div>
+        </div>
+      </section>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  import NavBar from '../components/navBar.vue';
+  
+  export default defineComponent({
+    name: 'IndexPage',
+    components: {
+      NavBar
+    }
+  });
+  </script>
+  
+  <style scoped>
+  .main-content {
+    position: relative;
+    text-align: center;
+    color: white;
+  }
+  
+  .hero-image {
+    width: 100%;
+    height: auto;
+  }
+  
+  .overlay {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background: rgba(0, 0, 0, 0.5);
+    padding: 20px;
+    border-radius: 10px;
+  }
+  
+  .btn {
+    display: inline-block;
+    padding: 10px 20px;
+    background-color: #00aaff;
+    color: #fff;
+    text-decoration: none;
+    border-radius: 5px;
+    margin-top: 10px;
+  }
+  
+  .btn:hover {
+    background-color: #0088cc;
+  }
+  </style>
+  
